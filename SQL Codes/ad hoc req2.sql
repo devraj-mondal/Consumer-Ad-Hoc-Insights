a223cc2583be39f9277b67ed2ba2f9d546dc56df
Devraj Mondal <64974135+devraj-mondal@users.noreply.github.com>
2025-02-05 00:33:46 +0530

    initial commit

--- SQL Codes/ad hoc req2.sql
+++ SQL Codes/ad hoc req2.sql
@@ -1,15 +1,5 @@
  /*What is the percentage of unique product increase in 2021 vs. 2020?*/
-SELECT 
-COUNT(DISTINCT CASE WHEN f.fiscal_year = 2020 THEN f.product_code END) AS unique_products_2020,
-COUNT(DISTINCT CASE WHEN f.fiscal_year = 2021 THEN f.product_code END) AS unique_products_2021,(
-(COUNT(DISTINCT CASE WHEN f.fiscal_year = 2021 THEN f.product_code END)-COUNT(DISTINCT CASE WHEN f.fiscal_year = 2020 THEN f.product_code END))*100
-/ NULLIF(COUNT(DISTINCT CASE WHEN f.fiscal_year = 2020 THEN f.product_code END), 0)
-)AS percentage_chg
-    FROM fact_sales_monthly f
-INNER JOIN dim_product d
-ON f.product_code = d.product_code;
-
-#CTE 
+#CTE#
 WITH my_tb AS (SELECT 
 COUNT(DISTINCT CASE WHEN f.fiscal_year = 2020 THEN f.product_code END) AS unique_products_2020,
 COUNT(DISTINCT CASE WHEN f.fiscal_year = 2021 THEN f.product_code END) AS unique_products_2021,ROUND(
