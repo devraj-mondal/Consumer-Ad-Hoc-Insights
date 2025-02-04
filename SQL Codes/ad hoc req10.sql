e095f2677ec53f85c20fa1ee9f7781e1321d714c
Devraj Mondal <64974135+devraj-mondal@users.noreply.github.com>
2025-02-05 00:31:48 +0530

    initial commit

--- SQL Codes/ad hoc req10.sql
+++ SQL Codes/ad hoc req10.sql
@@ -1,3 +1,11 @@
+
+/*10. Get the Top 3 products in each division that have a high
+total_sold_quantity in the fiscal_year 2021? The final output contains these
+fields,
+division
+product_code
+product
+total_sold_quantity*/
 #SELECT d.division,d.product_code,d.product, SUM(f.sold_quantity) AS total_sold_quantity
 #FROM dim_product d
 #INNER JOIN fact_sales_monthly f
