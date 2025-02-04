2ee7003eecb702f30e76332253082669af1c0181
Devraj Mondal <64974135+devraj-mondal@users.noreply.github.com>
2025-02-05 00:35:38 +0530

    initial commit

--- SQL Codes/ad hoc req4.sql
+++ SQL Codes/ad hoc req4.sql
@@ -1,3 +1,10 @@
+/*4. Follow-up: Which segment had the most increase in unique products in
+2021 vs 2020? The final output contains these fields,
+segment
+product_count_2020
+product_count_2021
+difference*/
+
 #SELECT segment, COUNT(DISTINCT CASE WHEN f.fiscal_year = 2020 THEN f.product_code END) AS products_2020,
 #COUNT(DISTINCT CASE WHEN f.fiscal_year = 2021 THEN f.product_code END) AS products_2021,
 #(COUNT(DISTINCT CASE WHEN f.fiscal_year = 2021 THEN f.product_code END)-COUNT(DISTINCT CASE WHEN f.fiscal_year = 2020 THEN f.product_code END)) AS difference
