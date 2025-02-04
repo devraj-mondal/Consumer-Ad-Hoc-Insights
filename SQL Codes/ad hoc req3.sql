5585659b73cb1569f4e55da81872effce7c5abf3
Devraj Mondal <64974135+devraj-mondal@users.noreply.github.com>
2025-02-05 00:34:57 +0530

    initial commit

--- SQL Codes/ad hoc req3.sql
+++ SQL Codes/ad hoc req3.sql
@@ -1,3 +1,8 @@
+/*3. Provide a report with all the unique product counts for each segment and
+sort them in descending order of product counts. The final output contains
+2 fields,
+segment
+product_count*/
 #SELECT segment, COUNT(DISTINCT product_code) AS Product_Count
 #FROM dim_product
 #Group by segment
