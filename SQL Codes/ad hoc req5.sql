fff22762a2baa1d04fa40d1523d91741e5022f51
Devraj Mondal <64974135+devraj-mondal@users.noreply.github.com>
2025-02-05 00:46:45 +0530

    initial commit

--- SQL Codes/ad hoc req5.sql
+++ SQL Codes/ad hoc req5.sql
@@ -1,3 +1,9 @@
+
+/*5. Get the products that have the highest and lowest manufacturing costs.
+The final output should contain these fields,
+product_code
+product
+manufacturing_cost*/
 #NORMAL QUERY
 #(
 #SELECT d.product_code, d.product, f.manufacturing_cost
