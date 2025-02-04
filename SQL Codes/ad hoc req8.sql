b724f73e05281e778df472baa07dc8efb1d94a9d
Devraj Mondal <64974135+devraj-mondal@users.noreply.github.com>
2025-02-05 00:49:58 +0530

    initial commit

--- SQL Codes/ad hoc req8.sql
+++ SQL Codes/ad hoc req8.sql
@@ -1,3 +1,8 @@
+/*8. In which quarter of 2020, got the maximum total_sold_quantity? The final
+output contains these fields sorted by the total_sold_quantity,
+Quarter
+total_sold_quantity*/
+
 WITH DerivedData AS (SELECT CASE
 WHEN MONTH(date) IN (9, 10, 11) THEN CONCAT(YEAR(date), '-Q1') 
 WHEN MONTH(date) IN (12, 1, 2) THEN CONCAT(YEAR(date) + CASE WHEN MONTH(date) = 12 THEN 0 ELSE -1 END, '-Q2')  
